b31d2ffe0de8404c0e38e0c9554509aecdcf57e9
griswoldthecat <griswoldthecat@users.noreply.github.com>
2022-03-30 23:28:57 +0000

    earthly 0.1648679212.89719916

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -1,8 +1,8 @@
 class Earthly < Formula
   desc "Build automation tool for the container era"
   homepage "https://earthly.dev/"
-  url "https://github.com/earthly/earthly-staging/archive/v0.1648668820.393435208.tar.gz"
-  sha256 "1a3302e6fc732ac4359266a805ae36c54b9f4471018459d3f5bd6cadcb3d6e16"
+  url "https://github.com/earthly/earthly-staging/archive/v0.1648679212.89719916.tar.gz"
+  sha256 "7dacff26b22f69c2dbeb7b820b545f3bc2ca739f65eed796ee96a651b7db2a28"
   license "BUSL-1.1"
   head "https://github.com/earthly/earthly-staging.git"
 
@@ -19,7 +19,7 @@ class Earthly < Formula
   depends_on "go@1.17" => :build
 
   def install
-    ldflags = "-X main.DefaultBuildkitdImage=docker.io/earthly/buildkitd:v#{version} -X main.Version=v#{version} -X main.GitSha=177358482ccd63eb46ae79d5be0ce4015af42f17 "
+    ldflags = "-X main.DefaultBuildkitdImage=docker.io/earthly/buildkitd:v#{version} -X main.Version=v#{version} -X main.GitSha=559046c09097a333428b07b4771c227a846128e5 "
     tags = "dfrunmount dfrunsecurity dfsecrets dfssh dfrunnetwork dfheredoc forceposix"
     system "go", "build",
         "-tags", tags,
