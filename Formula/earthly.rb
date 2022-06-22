26c0440ddc1f897ce4b567899bc9d2eb2eb67236
griswoldthecat <76011895+griswoldthecat@users.noreply.github.com>
2022-06-22 19:51:31 +0000

    earthly: update 0.1655925208.15179270 bottle.

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -12,8 +12,8 @@ class Earthly < Formula
   end
 
   bottle do
-    root_url "https://github.com/earthly/homebrew-earthly-staging/releases/download/earthly-0.1655918393.72944539"
-    sha256 cellar: :any_skip_relocation, big_sur: "583aa47a475555048afb00902443969211947566f4d1fd9d656a7cab21b41b83"
+    root_url "https://github.com/earthly/homebrew-earthly-staging/releases/download/earthly-0.1655925208.15179270"
+    sha256 cellar: :any_skip_relocation, big_sur: "1cffdb13bf17b2a42d07b3cb2072cc68ea695f90a5859cee3f8f624ff38eb936"
   end
 
   depends_on "go@1.17" => :build
